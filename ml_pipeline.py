cd86acf5935d4c25e8a2a1b359e9102b2ca6b326
Amy Wang <awang1596@gmail.com>
2021-02-17 20:27:14 -0700

    Shruti pair session

--- ml_pipeline.py
+++ ml_pipeline.py
@@ -8,19 +8,27 @@ from keras.layers.normalization import BatchNormalization
 from keras.utils import np_utils
 
 from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 
 def load_data():
-    return datasets.load_iris()
+    data = datasets.load_iris()
+    data_frame = pd.DataFrame(data= np.c_[data['data'], data['target']],
+                     columns= data['feature_names'] + ['target'])
+    return data_frame
 
-
-def split_data(data):
-    pass
+def split_data(data_frame):
+    x_train, x_test, y_train, y_test = train_test_split(
+                                            data_frame.iloc[:, 0:5].values,
+                                            data_frame['target'].values,
+                                            test_size = 0.2
+                                        )
+    return x_train, x_test, y_train, y_test
 
 
 def normalize(X_data):
-    return normalize(data, axis=0)
+    return normalize(X_data)
 
 
 def hot_encode(Y_data):
--- tests.py
+++ tests.py
@@ -3,26 +3,49 @@ import pytest
 import ml_pipeline
 
 
-@pytest.fixture
-def iris_dataset():
-    return datasets.load_iris()
 
 
-def describe_data():
-    def it_has_expected_features():
-        assert True == True
+# def describe_data():
+    # def it_has_expected_features():
+    #     data = ml_pipeline.load_data()
+    #     expected_columns = ["sepal length (cm)", "sepal width (cm)", "petal length (cm)", "petal width (cm)"]
+    #     for column in expected_columns:
+    #         assert column in data.columns
+
+    # def it_has_target():
+    #     data = ml_pipeline.load_data()
+    #     assert "target" in data.columns
+
 
 def describe_data_transformation():
-    pass
+    @pytest.fixture
+    def data():
+        return [[i] for i in range(0, 10)]
 
-def describe_training_test_split():
-    def it_splits_on_80_20():
-        assert True == False
 
-def describe_model():
-    def it_has_5_layers():
-        pass
+    @pytest.fixture
+    def normalized_data(data):
+        return [[i/10.0] for i in range(0, 10)]
 
-    def its_layers_have_weights():
-        pass
+
+    def it_normalizes_x_data(data, normalized_data):
+        assert ml_pipeline.normalize(data) == normalized_data
+
+
+
+# def describe_training_test_split():
+#     @pytest.fixture
+#     def iris_dataset():
+#         return ml_pipeline.load_data()
+
+#     def it_splits_on_80_20(iris_dataset):
+#         x_train, x_test, y_train, y_test = ml_pipeline.split_data(iris_dataset)
+#         assert len(y_train)/iris_dataset.shape[0] == 0.8
+
+# def describe_model():
+#     def it_has_5_layers():
+#         pass
+
+#     def its_layers_have_weights():
+#         pass
 
